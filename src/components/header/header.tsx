506e9bf6a167b5ebcd35520a23ebc87ed32f95da
Malin Persson <malin.persson02@hotmail.com>
2026-09-10 13:33:44 +0200

    Styled header to DD greybox

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,14 +5,14 @@ import { ShoppingBagIcon } from '@heroicons/react/24/outline';
 export function Header() {
   return (
     <header className={styles.header}>
-      <h1>LOGO</h1>
+      <h1 className={styles.logo}>LOGO</h1>
       <nav>
         <ul>
           <li><a href="#">Products</a></li>
           <li><a href="#">About us</a></li>
           <li><a href="#">Contact</a></li>
         </ul>
-        <ShoppingBagIcon />
+        <ShoppingBagIcon className={styles.shoppingBagIcon} />
       </nav>
     </header>
   )
