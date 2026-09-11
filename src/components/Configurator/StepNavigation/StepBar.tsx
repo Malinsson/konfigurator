678338465eb6bd167eac66b4f6364ce7bf11c8bb
Malin Persson <malin.persson02@hotmail.com>
2026-09-11 11:43:09 +0200

    Added basic stepBar styling

--- src/components/Configurator/StepNavigation/StepBar.tsx
+++ src/components/Configurator/StepNavigation/StepBar.tsx
@@ -3,10 +3,10 @@ import styles from './StepBar.module.css';
 export function StepBar() {
   return (
     <div className={styles.stepBar}>
-      <div className={styles.step}></div>
-      <div className={styles.step}></div>
-      <div className={styles.step}></div>
-      <div className={styles.step}></div>
+      <div className={styles.step}>1. Strap</div>
+      <div className={styles.step}>2. Dial Color</div>
+      <div className={styles.step}>3. Dial Style & Detail</div>
+      <div className={`${styles.step} ${styles.stepInactive}`}>4. Overview</div>
     </div>
   );
 }
